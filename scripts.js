94821f89cf050e5bb4fb0d343115a8847f110d89
roka32 <63305544+roka32@users.noreply.github.com>
2020-04-18 19:20:41 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -262,7 +262,7 @@ function myMoveDickAnimation() {
 var pos = 0;
 var id = setInterval(frame, 5);
 function frame() {
-  if (pos == 300) {
+  if (pos == 900) {
     $('#animate').css('visibility','hidden')
     clearInterval(id);
   } else {
